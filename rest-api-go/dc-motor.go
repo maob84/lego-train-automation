9541e77944bbac8f7565b8f628eda8607da232fc
Marcus Obst <marcus.obst@outlook.de>
2020-12-19 20:11:24 +0000

    feat: add first go implementation

---
+++ rest-api-go/dc-motor.go
@@ -0,0 +1,54 @@
+package main
+
+// TODO:
+//  * check pin mapping in rpio.go
+
+
+import (
+    "fmt"
+    "github.com/stianeikeland/go-rpio"
+)
+
+
+// map physical pins
+var (
+	pinIn1 = rpio.Pin(24)
+	pinIn2 = rpio.Pin(23)
+	pinEn = rpio.Pin(25)
+)
+
+
+func main() {
+
+	err := rpio.Open()
+	if err != nill {
+		panic(fmt.Sprint("unable to open gpio", err.Error()))
+	}
+
+	// shutdown memory access if we are done
+	defer rpio.Close()
+
+	// perform initial pin configuration
+	pinIn1.Output()
+	pinIn2.Output()
+	pinEn.Output()
+
+	// power down on start
+	pinIn1.Low()
+	pinIn2.Low()
+
+	// initialize PWM
+	pwm = pinEn.Pwm()
+	// We want to have 1000Hz at the output of the PWM with 25% duty cycle
+	pwm.Freq(1000 * 4)
+	pmw.DutyCycle(1,4)
+
+
+	// forward
+	pinIn1.High()
+	pinIn2.Low()
+
+	// backward
+	pinIn1.Low()
+	pinIn2.High()
+}
---
+++ rest-api-go/main.go
@@ -0,0 +1,45 @@
+// See https://tutorialedge.net/software-eng/designing-a-rest-api/
+
+
+package main
+
+import (
+    "fmt"
+    "log"
+    "net/http"
+    "encoding/json"
+    "github.com/stianeikeland/go-rpio"
+)
+
+func homePage(w http.ResponseWriter, r *http.Request){
+    fmt.Fprintf(w, "Welcome to the HomePage!")
+    fmt.Println("Endpoint Hit: homePage")
+}
+
+func handleRequests() {
+    http.HandleFunc("/", homePage)
+    http.HandleFunc("/trains", returnAllTrains)
+    log.Fatal(http.ListenAndServe(":10000", nil))
+}
+
+func returnAllTrains(w http.ResponseWriter, r *http.Request){
+	json.NewEncoder(w).Encode(Trains)
+}
+
+
+type Train struct {
+	Id int
+ 	Title string
+}
+
+
+var Trains []Train
+
+
+func main() {
+	Trains = []Train{
+		Train{Id: 1, Title: "Güterzug"},
+	}
+
+	handleRequests()
+}
