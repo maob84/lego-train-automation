120c641407ab0e5322f1a2141e68f1d4d5d9aff6
Marcus Obst <marcus.obst@outlook.de>
2020-12-23 23:37:19 +0100

    fix: run gofmt

--- rest-api-go/main.go
+++ rest-api-go/main.go
@@ -1,40 +1,36 @@
 // See https://tutorialedge.net/software-eng/designing-a-rest-api/
 
-
 package main
 
 import (
-    "fmt"
-    "log"
-    "net/http"
-    "encoding/json"
-    "github.com/stianeikeland/go-rpio"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	// "github.com/stianeikeland/go-rpio"
 )
 
-func homePage(w http.ResponseWriter, r *http.Request){
-    fmt.Fprintf(w, "Welcome to the HomePage!")
-    fmt.Println("Endpoint Hit: homePage")
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to the HomePage!")
+	fmt.Println("Endpoint Hit: homePage")
 }
 
 func handleRequests() {
-    http.HandleFunc("/", homePage)
-    http.HandleFunc("/trains", returnAllTrains)
-    log.Fatal(http.ListenAndServe(":10000", nil))
+	http.HandleFunc("/", homePage)
+	http.HandleFunc("/trains", returnAllTrains)
+	log.Fatal(http.ListenAndServe(":10000", nil))
 }
 
-func returnAllTrains(w http.ResponseWriter, r *http.Request){
+func returnAllTrains(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(Trains)
 }
 
-
 type Train struct {
-	Id int
- 	Title string
+	Id    int
+	Title string
 }
 
-
 var Trains []Train
-
 
 func main() {
 	Trains = []Train{
